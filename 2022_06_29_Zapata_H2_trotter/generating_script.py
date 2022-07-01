626ad18c826c99d7be3b4c8c2487a4f5032d95fb
Peter Johnson <peter.d.johnson22@gmail.com>
2022-07-01 09:01:49 -0400

    Updated python requirements file and made generating script to save hadamard test circuits

--- 2022_06_29_Zapata_H2_trotter/generating_script.py
+++ 2022_06_29_Zapata_H2_trotter/generating_script.py
@@ -1,33 +1,68 @@
-from matplotlib.pyplot import table
 import numpy as np
 from openfermion import QubitOperator
+import openfermion as of
+import openfermionpyscf as ofpyscf
 
-from zquantum.core.evolution import time_evolution
-from zquantum.core.circuits import Circuit, H
-from qeqiskit.conversions import import_from_qiskit, export_to_qiskit
+from orquestra.quantum.evolution import time_evolution
+from orquestra.quantum.circuits import Circuit, T
+from orquestra.integrations.qiskit.conversions import (
+    export_to_qiskit,
+)
 
 
 def estimate_number_of_trotter_steps(time, accuracy):
     # NOTE: this formula might be actually inaccurate
     # This might provide a better one: https://arxiv.org/abs/1912.08854
     # TODO: it should also depend on the norm of the Hamiltonian. Worth thinking about it future.
-    return int(np.ceil(time ** 2 / accuracy))
+    return int(np.ceil(time**2 / accuracy))
 
 
-def create_hadamard_test_circuit(unitary_circuit: Circuit) -> Circuit:
-    circuit = Circuit([H(0)])
-    # Build a controlled version of the unitry circuit
+def generate_h2_jw_qubit_hamiltonian():
+    # Set molecule parameters
+    geometry = [("H", (0.0, 0.0, 0.0)), ("H", (0.0, 0.0, 0.8))]
+    basis = "sto-3g"
+    multiplicity = 1
+    charge = 0
 
-    for op in unitary_circuit.operations:
-        # TODO: it's a hack exploiting particular Hamiltonian.
-        if op.gate.name == "H":
-            circuit += op
+    # Perform electronic structure calculations and
+    # obtain Hamiltonian as an InteractionOperator
+    hamiltonian = ofpyscf.generate_molecular_hamiltonian(
+        geometry, basis, multiplicity, charge
+    )
+
+    # Convert to a FermionOperator
+    hamiltonian_ferm_op = of.get_fermion_operator(hamiltonian)
+
+    # Map to QubitOperator using the JWT
+    hamiltonian_jw = of.jordan_wigner(hamiltonian_ferm_op)
+
+    return hamiltonian_jw
+
+
+def add_control_qubit_to_qubit_hamiltonian(qubit_hamiltonian, number_of_qubits):
+    # Ancilla qubit is set to be last qubit
+    # Create three terms, where we've ignored the identity term
+    # Positive 1/2 of Z on control qubit
+    ancilla_Z = 0.5 * QubitOperator(f"Z{number_of_qubits}")
+    # Positive 1/2 of I on control qubit and Hamiltonian on system
+    system_hamiltonian = 0.5 * qubit_hamiltonian
+    # Negative 1/2 of Z on control qubit and Hamiltonian on system
+    coupling_hamiltonian = qubit_hamiltonian * ancilla_Z
+    control_hamiltonian = ancilla_Z + system_hamiltonian + coupling_hamiltonian
+    # Make Hamiltonian coefficients real
+    control_hamiltonian.compress()
+    return control_hamiltonian
+
+
+def mock_transpile_clifford_t(circuit):
+    new_list = []
+    for gate_operation in circuit.operations:
+        if gate_operation.gate.name == "RZ":
+            new_list.append(T(gate_operation.qubit_indices[0]))
         else:
-            controlled_op = op.gate.controlled(1)
-            circuit += controlled_op(0, *[qubit + 1 for qubit in op.qubit_indices])
-
-    circuit += H(0)
-    return circuit
+            new_list.append(gate_operation)
+    new_circuit = Circuit(new_list)
+    return new_circuit
 
 
 def main():
@@ -35,19 +70,27 @@ def main():
     for time in [1]:
         for precision in [1e-2, 1e-3]:
             ### INPUTS ###
-            hamiltonian = QubitOperator("X0") + QubitOperator("Z0")
+            number_of_qubits = 4
+            qubit_hamiltonian = generate_h2_jw_qubit_hamiltonian()
+            control_hamiltonian = add_control_qubit_to_qubit_hamiltonian(
+                qubit_hamiltonian, number_of_qubits
+            )
 
+            # TODO: explain where this comes from
             trotter_error = precision / 10
 
             ### Prepare unitary circuit
             n_trotter_steps = estimate_number_of_trotter_steps(time, trotter_error)
             trotter_circuit = time_evolution(
-                hamiltonian, time=time, trotter_order=n_trotter_steps
+                control_hamiltonian, time=time, trotter_order=n_trotter_steps
             )
 
             ## Prepare algorithm circuit
-            circuit = create_hadamard_test_circuit(unitary_circuit=trotter_circuit)
-            qiskit_circuit = export_to_qiskit(circuit)
+            circuit = trotter_circuit
+            transpiled_circuit = mock_transpile_clifford_t(circuit)
+            # We may need to save to cirq for the ICM transpiling
+            # cirq_circuit = export_to_cirq(transpiled_circuit)
+            qiskit_circuit = export_to_qiskit(transpiled_circuit)
             file_name = f"time_{time}_error_{trotter_error}"
             file_name = file_name.replace(".", "_") + ".txt"
             with open(file_name, "w") as f:
